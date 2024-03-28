c38f1544109115200d4c23d71eaa078c387b3af2
JustinSheng <justin.wang4@icloud.com>
2024-03-28 15:01:33 +0800

    create collection view cell

---
+++ ColletionView/NumberCell.swift
@@ -0,0 +1,14 @@
+//
+//  NumberCell.swift
+//  ColletionView
+//
+//  Created by Justin on 2024/3/28.
+//
+
+import UIKit
+
+class NumberCell: UICollectionViewCell {
+	static let reuseIdentifier = String(describing: NumberCell.self)
+	
+	@IBOutlet weak var label: UILabel!
+}
