efa506049d5211402685d4609adc99654d4f7328
JustinSheng <justin.wang4@icloud.com>
2024-03-28 15:25:34 +0800

    Creating Data Source and SnapShots

--- ColletionView/ViewController.swift
+++ ColletionView/ViewController.swift
@@ -9,12 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 	
+	enum Section {
+		case main
+	}
+	
 	@IBOutlet weak var collectionView: UICollectionView!
+	
+	var dataSource: UICollectionViewDiffableDataSource<Section, Int>!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		collectionView.collectionViewLayout = configureLayout()
+		configureDataSource()
 	}
 	
 	func configureLayout() -> UICollectionViewCompositionalLayout {
@@ -26,6 +33,25 @@ class ViewController: UIViewController {
 		
 		let section = NSCollectionLayoutSection(group: group)
 		return UICollectionViewCompositionalLayout(section: section)
+	}
+	
+	func configureDataSource() {
+		dataSource = UICollectionViewDiffableDataSource(collectionView: self.collectionView) {
+			(collectionView, indexPath, number) -> UICollectionViewCell? in
+			guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NumberCell.reuseIdentifier, for: indexPath) as? NumberCell else {
+				fatalError("Cannot create new cell!")
+			}
+			
+			cell.label.text = number.description
+			
+			return cell
+		}
+		
+		var initialSnapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+		initialSnapshot.appendSections([.main])
+		initialSnapshot.appendItems(Array(1...100))
+		
+		dataSource.apply(initialSnapshot, animatingDifferences: false)
 	}
 	
 	
